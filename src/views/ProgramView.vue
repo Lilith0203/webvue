13a07ff56e7ca7a5233bfeb2f18bf45507973d53
lily_vain <hulili@purvar.com>
2024-12-30 16:23:12 +0800

    添加作品

--- src/views/ProgramView.vue
+++ src/views/ProgramView.vue
@@ -5,16 +5,16 @@ const apps = ref([
   {
     id: 1,
     title: '妙手回春---Web游戏',
-    description: '大学时期作品',
+    description: '大学时期作品，仅支持pc端',
     image: '/images/03092015013116.png',
-    link: 'https://lilysvain.github.io/works/spring/index.html'
+    link: '/programs/spring/index.html'
   },
   {
     id: 2,
     title: '圣诞Jump---Web游戏',
     description: '大学时期作品',
     image: '/images/03092015014024.png',
-    link: 'https://lilysvain.github.io/works/santa/'
+    link: '/programs/santa/index.html'
   }
 ])
 </script>
@@ -26,7 +26,7 @@ const apps = ref([
           <div class="info">
             <h2 class="app-title">{{ app.title }}</h2>
             <p class="app-description">{{ app.description }}</p>
-            <a :href="app.link" class="app-link" target="_blank">查看详情</a>
+            <p><a :href="app.link" class="app-link" target="_blank">查看详情</a></p>
           </div>
           <div>
             <img :src="app.image" alt="App Image" class="app-image" />
@@ -58,7 +58,7 @@ const apps = ref([
 }
 
 .info h2 {
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   font-weight: bold;
 }
 
@@ -67,6 +67,19 @@ const apps = ref([
   min-height: 100px;
   border: 2px solid #fff;
   box-shadow: 0px 0px 3px rgba(0,0,0,0.2);
+}
+
+.app-link {
+  display: inline-block;
+  background: transparent url(/images/wlink-bg.png) no-repeat center right;
+  color: #d55355;
+  width: 90px;
+  transition: 0s;
+}
+
+.app-link:hover {
+  color: #4c9ed9;
+  background-position: left;
 }
 
 @media (min-width: 1024px) {
