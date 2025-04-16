00ef98c96abd791794461b248e3cfde9fec7b370
lily_vain <hulili@purvar.com>
2025-04-16 11:52:00 +0800

    试试缩略图锐化

--- src/views/StoryView.vue
+++ src/views/StoryView.vue
@@ -712,8 +712,8 @@ const getThumbnailUrl = (url, width = 240) => {
   if (!url) return '';
   
   // 使用更高的锐化参数
-  return `${url}?x-oss-process=image/auto-orient,1`
-    + `/resize,w_${width},m_lfit` // 限制宽度，保持原始比例
+  return `${url}?x-oss-process=image`
+    + `/resize,w_${width}` // 限制宽度，保持原始比例
     + `/sharpen,100` // 增加锐化程度到100
 }
 
